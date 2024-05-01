dd3bd94f85a71f82c1987ac0022eee7f201fa10f
Tomek <jasinskitomasz15@gmail.com>
2024-05-01 11:24:03 +0200

    2222

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 	
 	public static void main(String[] args) {      
-	        qpa                      
+	        qpa 22222                     
 		System.out.println("Hello World");
 	}
 }
